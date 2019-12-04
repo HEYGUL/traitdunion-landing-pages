4699b18dc3c75f40bf405d9e0ceee81884ca48f9
Guillaume Lagorce <glagorce@octo.com>
2019-12-04 17:13:22 +0100

    Fix redirect

--- server.js
+++ server.js
@@ -1,30 +1,32 @@
-var express = require('express')
+const express = require('express')
 
-var app = express()
-var directory = '/' + (process.env.STATIC_DIR || 'public')
-app.use(express.static(__dirname + directory))
-
-var port = process.env.PORT || 3000;
+const app = express()
+const port = process.env.PORT || 3000;
 
 // RedirectPermanent / https://traitdunion.beta.gouv.fr/?utm_source=oldsite
 app.get('/', (req, res) => {
-  res.redirect('https://traitdunion.beta.gouv.fr/?utm_source=oldsite')
+  console.log('/')
+  res.redirect(301, 'https://traitdunion.beta.gouv.fr/?utm_source=oldsite')
 })
 
 // RedirectPermanent /candidats/ https://traitdunion.beta.gouv.fr/candidats/?utm_source=oldsite
 app.get('/candidats/', (req, res) => {
-  res.redirect('https://traitdunion.beta.gouv.fr/candidats/?utm_source=oldsite')
+  res.redirect(301, 'https://traitdunion.beta.gouv.fr/candidats/?utm_source=oldsite')
 })
 
 // RedirectPermanent /candidats/liste-pmsmp.html https://traitdunion.beta.gouv.fr/offres?utm_source=oldsite
 app.get('/candidats/list-pmsmp.html', (req, res) => {
-  res.redirect('https://traitdunion.beta.gouv.fr/offres?utm_source=oldsite')
+  res.redirect(301, 'https://traitdunion.beta.gouv.fr/offres?utm_source=oldsite')
 })
 
 // RedirectPermanent /entreprises/ https://traitdunion.beta.gouv.fr/entreprises?utm_source=oldsite
 app.get('/entreprises/', (req, res) => {
-  res.redirect('https://traitdunion.beta.gouv.fr/entreprises?utm_source=oldsite')
+  res.redirect(301, 'https://traitdunion.beta.gouv.fr/entreprises?utm_source=oldsite')
 })
+
+app.use(function forceLiveDomain(req, res, next) {
+  return res.redirect(301, 'https://traitdunion.beta.gouv.fr/?utm_source=oldsite')
+});
 
 app.listen(port, function () {
   console.log('Listening on', port)
